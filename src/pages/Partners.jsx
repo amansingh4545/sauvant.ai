ff9b00534fc316d6735d8005b44e83580cb12a1f
amansingh4545 <133216664+amansingh4545@users.noreply.github.com>
2024-02-26 22:50:43 +0530

    Update Partners.jsx

--- src/pages/Partners.jsx
+++ src/pages/Partners.jsx
@@ -4,7 +4,7 @@ import { Button, Img, Input, Line, Text } from "components";
 export default function Partners(){
     return(
         <>
-            <div style={{width:"90%", margin:"auto"}} className="bg-white-A700 flex flex-col gap-6 items-center justify-start max-w-[1438px] md:px-10 px-20 sm:px-5 py-[60px] w-full">
+            <div style={{width:"100%", margin:"auto"}} className="bg-white-A700 flex flex-col gap-6 items-center justify-start max-w-[1438px] md:px-10 px-20 sm:px-5 py-[60px] w-full">
             <div className="flex flex-col gap-4 items-center justify-start w-auto md:w-full">
               <Text
                 className="text-2xl md:text-[22px] text-center text-gray-900_01 sm:text-xl w-auto"
@@ -29,44 +29,44 @@ export default function Partners(){
               </Text>
             </div>
             <div className="gap-[30px] grid sm:grid-cols-1 md:grid-cols-3 grid-cols-6 items-start justify-start max-w-7xl mx-auto w-full">
-              <div className="bg-white-A700 flex flex-1 flex-col items-center justify-start md:px-10 sm:px-5 px-[53px] w-full">
+              <div className="bg-white-A700 flex flex-1 flex-col items-center justify-start md:px-10 sm:px-5 px w-full">
                 <Img
-                  className="h-[100px] md:h-auto object-cover w-full"
+                  className="h md:h-auto object-cover w-full"
                   src="images/img_rblbanklogo1.png"
                   alt="rblbanklogoOne"
                 />
               </div>
-              <div className="bg-white-A700 flex flex-1 flex-col items-center justify-start md:px-10 sm:px-5 px-[51px] w-full">
+              <div className="bg-white-A700 flex flex-1 flex-col items-center justify-start md:px-10 sm:px-5 px w-full">
                 <Img
-                  className="h-[100px] md:h-auto object-cover w-full"
+                  className="h md:h-auto object-cover w-full"
                   src="images/img_zype1.png"
                   alt="zypeOne"
                 />
               </div>
-              <div className="bg-white-A700 flex flex-1 flex-col items-start justify-start sm:px-5 px-9 w-full">
+              <div className="bg-white-A700 flex flex-1 flex-col items-start justify-start sm:px-5 px w-full">
                 <Img
-                  className="h-[100px] md:h-auto object-cover w-[87%]"
+                  className="h h-[140px] md:h-[140px] object-cover w-[87%]"
                   src="images/img_ackologo1.png"
                   alt="ackologoOne"
                 />
               </div>
-              <div className="bg-white-A700 flex flex-1 flex-col items-center justify-start md:px-10 px-12 sm:px-5 w-full">
+              <div className="bg-white-A700 flex flex-1 flex-col items-center justify-start md:px-10 px sm:px-5 w-full">
                 <Img
-                  className="h-[100px] md:h-auto object-cover w-full"
+                  className="h md:h-auto object-cover w-full"
                   src="images/img_incredlogo1.png"
                   alt="incredlogoOne"
                 />
               </div>
-              <div className="bg-white-A700 h-[100px] sm:px-5 px-[38px] relative w-full">
+              <div className="bg-white-A700 h-[140px] sm:px-5 px relative w-full">
                 <Img
-                  className="absolute h-[100px] inset-[0] justify-center m-auto object-cover w-3/5"
+                  className="absolute h inset-[0] justify-center m-auto object-cover w-3/5"
                   src="images/img_cholamandalamlogo1.png"
                   alt="cholamandalamlo"
                 />
               </div>
-              <div className="bg-white-A700 h-[100px] sm:px-5 px-[38px] relative w-full">
+              <div className="bg-white-A700 h-[150px] sm:px-5 px relative w-full">
                 <Img
-                  className="absolute h-12 inset-y-[0] left-[18%] my-auto object-cover w-[79%]"
+                  className="absolute h-12 inset-y-[0] left-[18%] my-auto object-cover w"
                   src="images/img_croppedghostk.png"
                   alt="croppedghostk"
                 />
