71d646e513b609d54eeb6dca07c3767f80a43f1f
amansingh4545 <133216664+amansingh4545@users.noreply.github.com>
2024-02-26 22:50:10 +0530

    Update Header.jsx

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -6,8 +6,8 @@ import "./Header.css";
 function Header(){
     return (
         <>
-        <header className="H1">
-          <div className="H2">
+        <header  style={{width:"100%"}} className="H1">
+          <div  className="H2">
             <div className="H3">
               <Img className="H4" src="images/img_creditsea1.png" alt="creditseaOne" />
               <div className="H5">
@@ -15,12 +15,12 @@ function Header(){
                 <Text className="H7" size="txtMories24"> TRUST. COMFORT. PROSPER </Text>
               </div>
             </div>
-            <div className="md:ml-[0] ml-[165px]">
+            <div>
               <div className="H8">
-                  <Link to="#" > Home</Link>
-                  <Link to="#" > How It Works</Link>
-                  <Link to="#" > Why CREDITSEA</Link>
-                  <Link to="#" > Blog</Link>
+                  <Link className="H12" to="#" > Home</Link>
+                  <Link className="H12" to="#" > How It Works</Link>
+                  <Link className="H12" to="#" > Why CREDITSEA</Link>
+                  <Link className="H12" to="#" > Blog</Link>
               </div>
             </div>
             <Link to="#" className="H9"> Login/Signup </Link>
